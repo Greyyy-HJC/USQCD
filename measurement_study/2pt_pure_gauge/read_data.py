b030e8d4b8907baa17d247a280bd4535235d50b0
Greyyy-HJC <hejinchen17@gmail.com>
2023-08-27 10:38:40 -0400

    100_S16_T32_configs

--- measurement_study/2pt_pure_gauge/read_data.py
+++ measurement_study/2pt_pure_gauge/read_data.py
@@ -7,11 +7,10 @@ from read_2pt_gamma_module import *
 
 data_list = []
 
-for conf_num in range(1, 11):
+for conf_num in range(1, 101, 50):
     file_path = 'out_xml/pure_gauge_meson_2pt_cfg{}.out.xml'.format(conf_num)
 
-    data_list.append([])
-    data_list[conf_num - 1] = read_real_data_for_gamma(file_path, gamma_val=15)
+    data_list.append( read_real_data_for_gamma(file_path, gamma_val=15) )
 
 print(np.shape(data_list))
 
@@ -21,8 +20,9 @@ print(np.shape(data_list))
 from liblattice.preprocess import resampling as resam
 from liblattice.general import general_plot_funcs as gplt
 
-data_resam = resam.bootstrap(data_list, samp_times=50)
-data_avg = gv.dataset.avg_data(data_resam, bstrap=True)
+data_resam = resam.bootstrap(data_list, samp_times=200)
+# data_avg = gv.dataset.avg_data(data_resam, bstrap=True)
+data_avg = gv.dataset.avg_data(data_list, bstrap=False)
 
 print(data_avg)
 
@@ -36,4 +36,7 @@ title = 'Effective mass for pure gauge theory'
 gplt.errorbar_plot(x, y, yerr, title, save=False, ylim=[0.5,4])
 
 
+# %%
+# save the data list as a txt file
+# np.savetxt('data_list.txt', data_list, fmt='%s')
 # %%
